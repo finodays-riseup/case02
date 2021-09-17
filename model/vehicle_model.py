7498745d1de6ee2271592e8a2ae6a201d5523f4a
Chera <valiev_chermen@mail.ru>
2021-09-17 23:07:33 +0300

    vehicle_model.py

--- model/vehicle_model.py
+++ model/vehicle_model.py
@@ -1,9 +1,14 @@
 from model.pickled_model import PickledModel
-
+import numpy as np
+import pandas as pd
 
 class VehicleModel(PickledModel):
     def _encode_features(self, features):
-        pass
+        features["age"] = 2021 - features["year"]
+        features.pop("year", None)
+        features.pop("model", None)
+        features["complectation"] = len(features["complectation"].split(','))
+        return pd.DataFrame([features])
 
     def _decode_target(self, target):
-        pass
+        return np.exp(target)[0]
