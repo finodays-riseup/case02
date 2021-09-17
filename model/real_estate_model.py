209ac2af0b8a8db8f750f26de0839876c0d22449
Alexander Laptev <alex.laptev2000@yandex.ru>
2021-09-17 20:26:18 +0300

    Added real estate model wrapper

--- model/real_estate_model.py
+++ model/real_estate_model.py
@@ -1,9 +1,56 @@
-from model.pickled_model import PickledModel
+from model import Model
+import numpy as np
+import pandas as pd
+from geopy.distance import geodesic
 
 
-class RealEstateModel(PickledModel):
-    def _encode_features(self, features):
-        return
+class RealEstateModel(Model):
+    COLUMNS = [
+        'geo_lat', 'geo_lon', 'building_type', 'level', 'levels', 'rooms',
+        'area', 'kitchen_area', 'city_center_distance', 'population'
+    ]
 
-    def _decode_target(self, target):
-        pass
+    MATERIALS_ENCODING = {
+        "Другое": 0,
+        "Панельный": 1,
+        "Монолитный": 2,
+        "Кирпичный": 3,
+        "Блочный": 4,
+        "Деревянный": 5,
+    }
+
+    def __init__(self, model_path, cities_path):
+        super().__init__(model_path)
+        self._cities = pd.read_csv(cities_path)
+
+    def __init__(self, path):
+        super().__init__(path)
+
+    def encode_features(self, features):
+        encoded_features = {}
+        for column in RealEstateModel.COLUMNS:
+            if column in features:
+                encoded_features[column] = features[column]
+            else:
+                encoded_features[column] = None
+        encoded_features['building_type'] = RealEstateModel.MATERIALS_ENCODING[
+            encoded_features['building_type']]
+        if features['is_studio']:
+            encoded_features['rooms'] = 0
+
+        distance = float("+inf")
+        population = 0
+        coordinates = (features['geo_lat'], features['geo_lon'])
+        for (_, city) in self._cities.iterrows():
+            cur_distance = geodesic((city['geo_lat'], city['geo_lon']),
+                                    coordinates)
+            if cur_distance < distance:
+                distance = cur_distance
+                population = city['population']
+        encoded_features['population'] = population
+        encoded_features['city_center_distance'] = distance
+
+        return pd.DataFrame.from_dict(encoded_features)
+
+    def decode_target(self, target):
+        return np.exp(target)[0]
