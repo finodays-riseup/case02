33e27ade97d0f5947571a6bf20e3e06e457f6991
Stanislav Tebloev <stas.tebloev@yandex.ru>
2021-09-17 13:23:48 +0300

    Add model integration

--- ui/pages/result_page.py
+++ ui/pages/result_page.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
+from model.model import Model
 from ui.pages.abstract_page import AbstractPage
+
+
+@st.cache
+def get_real_estate_model() -> Model:
+    raise NotImplementedError()
+
+
+@st.cache
+def get_vehicle_model() -> Model:
+    raise NotImplementedError()
 
 
 class ResultPage(AbstractPage):
@@ -11,5 +22,11 @@ class ResultPage(AbstractPage):
         rt = self._root
 
         data = st.session_state["form_data"]
-        rt.write(data)
+        model = None
+        if data["property_type"] == "Недвижимость":
+            model = get_real_estate_model()
+        elif data["property_type"] == "Автомобиль":
+            model = get_vehicle_model()
+
+        rt.write(model.predict(data))
         rt.button("Назад", on_click=self._update_state)
